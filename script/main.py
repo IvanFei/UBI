d218c6dd4d88953e4cd7badbb326f550228d0771
fei <huangfeifei5943@sina.cn>
2018-11-07 15:10:55 +0800

    ubi predict

---
+++ script/main.py
@@ -0,0 +1,179 @@
+# -*- coding:utf8 -*-
+import os
+import csv
+import pandas as pd
+import numpy as np
+import sklearn
+import scipy as sp
+import time
+import random
+import xgboost
+import warnings
+warnings.filterwarnings("ignore")
+
+# model 
+from sklearn import linear_model, svm, ensemble, neighbors
+from sklearn import preprocessing
+from sklearn import model_selection
+from sklearn import metrics
+from xgboost import XGBRegressor
+
+# 导入模型
+from model import *
+
+## 文件地址
+path_train = "/data/dm/train.csv"  # 训练文件
+path_test = "/data/dm/test.csv"  # 测试文件
+
+path_test_out = "model/"  # 预测结果输出路径为model/xx.csv,有且只能有一个文件并且是CSV格式。
+
+columns = ['TERMINALNO', 'TIME', 'TRIP_ID', 'LONGITUDE', 'LATITUDE', 'DIRECTION', 'HEIGHT', 'SPEED', 'CALLSTATE']
+target = ['Y']
+
+def read_csv(path, n_rows=None):
+    """
+    文件读取模块，头文件见columns.
+    :return: 
+    """
+    # for filename in os.listdir(path_train):
+    tempdata = pd.read_csv(path, nrows=n_rows)
+    #tempdata.columns = ["TERMINALNO", "TIME", "TRIP_ID", "LONGITUDE", "LATITUDE", "DIRECTION", "HEIGHT", "SPEED",
+    #                   "CALLSTATE", "Y"]
+    return tempdata
+
+
+def process(pred):
+    """
+    处理过程，在示例中，使用随机方法生成结果，并将结果文件存储到预测结果路径下。
+    :return: 
+    """
+    
+    count = 0
+    with open(path_test) as lines:
+        with(open(os.path.join(path_test_out, "test.csv"), mode="w")) as outer:
+            writer = csv.writer(outer)
+            i = 0
+            ret_set = set([])
+            for line in lines:
+                if i == 0:
+                    i += 1
+                    writer.writerow(["Id", "Pred"])  # 只有两列，一列Id为用户Id，一列Pred为预测结果(请注意大小写)。
+                    continue
+                item = line.split(",")
+                if item[0] in ret_set:
+                    continue
+                # 此处使用随机值模拟程序预测结果
+                #writer.writerow([item[0], np.random.rand()]) # 随机值
+                writer.writerow([item[0], pred[count]]) # 随机值
+                count += 1
+                ret_set.add(item[0])  # 根据赛题要求，ID必须唯一。输出预测值时请注意去重
+                
+
+
+# range feature// feature extract
+def rangeFeat(arr):
+    
+    return arr.max() - arr.min()
+
+def calcStd(arr):
+    
+    if (arr.shape[0] == 1):
+        return 0
+    else:
+        return arr.std()
+    
+def mode(arr):
+    
+    return sp.stats.mode(arr, axis=None)[0]
+
+# preprocess
+def dataXPreprocess(df):
+    """
+    数据预处理。
+    特征的提取，归一化，数据的导入
+    """
+     ## number of sample
+    terminalno = set(df[columns[0]])
+    numSample = len(terminalno)
+    print('Number of train/test data: {}'.format(numSample))
+    numFeat = 10
+    
+    #print(df.isnull().sum())
+    df[df == -1] = np.NAN
+    print('Number of -1 value:')
+    print(df.isnull().sum())
+    df = df.fillna(method='bfill')
+    
+    # data and target
+    dataX = df[columns[:]]
+    
+    #提取特征包括 mean/max/std/mode
+    dataFeatExtract = dataX.groupby([columns[0]]).agg(
+            {'TIME': {'sum': 'mean'}, 'TRIP_ID': {'max': 'max'}, 
+             'LONGITUDE': {'mean': 'mean'}, 'LATITUDE': {'mean': 'mean'},
+             'DIRECTION': {'std': calcStd}, 'HEIGHT': {'mean': 'mean', 'std': calcStd}, 
+             'SPEED': {'mean': 'mean', 'std': calcStd}, 'CALLSTATE': {'mode':  mode}})
+    
+    #提取每个行程的时间跨度后加和为驾驶的时间
+    Time = dataX.groupby([columns[0], columns[2]]).agg({'TIME': {'rangeFeat': rangeFeat}})
+    T = np.zeros([numSample])
+    for i, ind in enumerate(Time.index.levels[0]):
+        T[i] = np.array(Time.xs(ind)).sum()
+        
+    dataFeatExtract['TIME'] = T
+    
+    #转换成ndarray 
+    dataCollect = np.array(dataFeatExtract)
+    
+    # Normalization
+    X = np.zeros([numSample, numFeat])
+    X[:, :-1] = preprocessing.StandardScaler().fit_transform(dataCollect[:, :-1])
+    X[:, -1] = dataCollect[:, -1]
+    
+    return X
+
+def dataYPreprocess(df):
+    
+    #产生Y
+    Y = np.array(df.groupby(columns[0], as_index=False).mean()['Y'])
+    return Y
+
+def modelFit(modelSet, X, Y):
+    
+    #训练模型集
+    for rlf in modelSet:
+        rlf[1].fit(X, Y)
+    
+    print("Model fit finished!!!")
+    return modelSet
+
+def modelPredict(modelSet, X):
+    
+    #预测
+    numMode = len(modelSet)
+    numSample = X.shape[0]
+    pred = np.zeros([numSample, numMode])
+    for i, rlf in enumerate(modelSet):
+        pred[:, i] = rlf[1].predict(X)
+    
+    print("Model predict finished!!!") 
+    return np.mean(pred, axis=1)
+    
+
+if __name__ == "__main__":
+    print("****************** start **********************")
+    # 程序入口
+    data = read_csv(path_train, n_rows=None) #6000000
+    print(data.info())
+    X = dataXPreprocess(data)
+    Y = dataYPreprocess(data)
+    #参数选择
+    rlfSet = model(X, Y)
+    # 训练模型
+    rlfSet = modelFit(rlfSet, X, Y)
+    testData = read_csv(path_test)
+    testX = dataXPreprocess(testData)
+    #预测
+    pred = modelPredict(rlfSet, testX)
+    
+    process(pred)
---
+++ script/model.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sklearn
+import scipy as sp
+
+# model 
+from sklearn import linear_model, svm, ensemble, neighbors, neural_network
+from sklearn import preprocessing
+from sklearn import model_selection
+from sklearn import metrics
+from xgboost import XGBRegressor
+
+def model(X, Y):
+    
+#    MLA = [
+#    # ensemble Model
+#    ensemble.AdaBoostRegressor(),
+#    ensemble.GradientBoostingRegressor(),
+#    ensemble.ExtraTreesRegressor(), 
+#    
+#    #GLM
+#    linear_model.SGDRegressor(),
+#    
+#    #SVM
+#    svm.NuSVR(),
+#    svm.SVR(),
+#    
+#    #xgboost
+#    XGBRegressor()
+#    ]
+
+    # data splite
+    split = model_selection.ShuffleSplit(n_splits=3, random_state=0)
+    
+    grid_n_estimator = [100, 300, 600, 1000, 1300]
+    grid_ratio = [.1, .25, .5, .75, 1.0]
+    grid_learn = [.01, .03, .05, .1]
+    grid_max_depth = [2, 4, 8, None]
+    #grid_criterion = ['gini', 'entropy']
+    grid_seed = [0]
+    n_jobs = 2
+
+    vote_est = [
+        #Ensemble Methods: http://scikit-learn.org/stable/modules/ensemble.html
+        #('ada', ensemble.AdaBoostRegressor()),
+        #('bc', ensemble.BaggingRegressor(n_jobs=n_jobs)),
+        ('gbc', ensemble.GradientBoostingRegressor()),
+        #('rfc', ensemble.RandomForestRegressor(n_jobs=n_jobs)),
+        #('etc',ensemble.ExtraTreesRegressor(n_jobs=n_jobs)),
+        
+        #SVM: http://scikit-learn.org/stable/modules/svm.html
+        #('svc', svm.SVR()),
+        
+        #MLPRegressor
+        ('mlp', neural_network.MLPRegressor(hidden_layer_sizes=(50, 100, 20,), max_iter=2000)),
+        
+        #xgboost: http://xgboost.readthedocs.io/en/latest/model.html
+        ('xgb', XGBRegressor())
+    
+    ]
+    
+    grid_param = [
+                    #[{
+                    ##AdaBoostRegressor - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html
+                    #'n_estimators': grid_n_estimator, #default=50
+                    #'learning_rate': grid_learn, #default=1
+                    #'algorithm': ['SAMME', 'SAMME.R'], #default=’SAMME.R
+                    #'random_state': grid_seed
+                    #}],
+           
+        
+                    #[{
+                    ##BaggingRegressor - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.BaggingClassifier.html#sklearn.ensemble.BaggingClassifier
+                    #'n_estimators': grid_n_estimator, #default=10
+                    #'max_samples': grid_ratio, #default=1.0
+                    #'random_state': grid_seed
+                    #}],
+    
+    
+                    [{
+                    #GradientBoostingRegressor - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingClassifier.html#sklearn.ensemble.GradientBoostingClassifier
+                    #'loss': ['deviance', 'exponential'], #default=’deviance’
+                    'learning_rate': [.05], #default=0.1 -- 12/31/17 set to reduce runtime -- The best parameter for GradientBoostingClassifier is {'learning_rate': 0.05, 'max_depth': 2, 'n_estimators': 300, 'random_state': 0} with a runtime of 264.45 seconds.
+                    'n_estimators': [300], #default=100 -- 12/31/17 set to reduce runtime -- The best parameter for GradientBoostingClassifier is {'learning_rate': 0.05, 'max_depth': 2, 'n_estimators': 300, 'random_state': 0} with a runtime of 264.45 seconds.
+                    #'criterion': ['friedman_mse', 'mse', 'mae'], #default=”friedman_mse”
+                    'max_depth': grid_max_depth, #default=3   
+                    'random_state': grid_seed
+                     }],
+    
+        
+                    #[{
+                    ##RandomForestRegressor - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier
+                    #'n_estimators': grid_n_estimator, #default=10
+                    ##'criterion': grid_criterion, #default=”gini”
+                    #'max_depth': grid_max_depth, #default=None
+                    #'oob_score': [True], #default=False -- 12/31/17 set to reduce runtime -- The best parameter for RandomForestClassifier is {'criterion': 'entropy', 'max_depth': 6, 'n_estimators': 100, 'oob_score': True, 'random_state': 0} with a runtime of 146.35 seconds.
+                    #'random_state': grid_seed
+                    # }],
+        
+                    #   [{
+                    ##ExtraTreesRegressor - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.ExtraTreesClassifier.html#sklearn.ensemble.ExtraTreesClassifier
+                    #'n_estimators': grid_n_estimator, #default=10
+                    ##'criterion': grid_criterion, #default=”gini”
+                    #'max_depth': grid_max_depth, #default=None
+                    #'random_state': grid_seed
+                    # }],
+        
+               
+        
+                    #[{
+                    ##SVR - http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
+                    ##http://blog.hackerearth.com/simple-tutorial-svm-parameter-tuning-python-r
+                    ##'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+                    #'C': [1,2,3,4,5], #default=1.0
+                    #'gamma': grid_ratio, #edfault: auto
+                    # }],
+    
+                    [{
+                    #MLP regressor       
+                    'activation': ['logistic'],
+                    'random_state': grid_seed,
+                    }],
+    
+        
+                    [{
+                    #XGBRegressor - http://xgboost.readthedocs.io/en/latest/parameter.html
+                    'learning_rate': grid_learn, #default: .3
+                    'max_depth': [1,3,7], #default 2
+                    'n_estimators': grid_n_estimator, 
+                    'seed': grid_seed  
+                     }]
+    ]
+
+    for rlf, param in zip(vote_est, grid_param):
+        
+        best_search = model_selection.GridSearchCV(estimator=rlf[1], 
+                param_grid=param, cv=split, scoring='neg_mean_squared_error', n_jobs=n_jobs)
+        best_search.fit(X, Y)
+        
+        best_param = best_search.best_params_
+        bestIndex = best_search.best_index_
+        trainBestScore = best_search.cv_results_['mean_train_score'][bestIndex]
+        testBestScore = best_search.best_score_
+        print('The best parameter for {} is {}.'.format(rlf[1].__class__.__name__, best_param))
+        print('The train best score {:.3f}'.format(trainBestScore))
+        print('The test best score {:.3f}'.format(testBestScore))
+        
+        rlf[1].set_params(**best_param)
+        
+    return vote_est
+        
+        
